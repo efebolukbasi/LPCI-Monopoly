5c1b876a8ffc7b0859ceebf074ca6953cc335ca5
OliverSM95 <olisimm2006@gmail.com>
2023-05-18 15:23:04 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        boolean gameMode = true; // Main Game While Loop Condition
 
         System.out.print("How many players: ");
         int numPlayers = in.nextInt();
 
+        System.out.print("Select Game Length:\n1.Short\n2.Medium\n3.Long\nEnter: ");
+        int gameLength = in.nextInt();
+        while(gameLength <=0 || gameLength >=4){
+            System.out.print("Select Game Length:\n1.Short\n2.Medium\n3.Long\nEnter: ");
+            gameLength = in.nextInt();
+        }
 
+
+        while (gameMode){
+
+
+
+        }
 
     }
 }
