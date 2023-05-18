2c68f0905d69896d57c6b671ac3e329f160ed5ba
unknown <345025845@TDSB.ON.CA>
2023-05-18 09:37:11 -0400

    Files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How many players: ");
+        int numPlayers = in.nextInt();
+
+    }
+}
