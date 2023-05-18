a7ca88de9c28d2cda0a8fca16a00cc0068d4fc84
OliverSM95 <olisimm2006@gmail.com>
2023-05-18 15:23:04 -0400

    test

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,27 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Main {
-    public static boolean user1Turn = true;
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-         final int G_COINS =8; // each player has 8 coins
+        boolean gameMode = true; // Main Game While Loop Condition
+
+        System.out.print("How many players: ");
+        int numPlayers = in.nextInt();
+
+        System.out.print("Select Game Length:\n1.Short\n2.Medium\n3.Long\nEnter: ");
+        int gameLength = in.nextInt();
+        while(gameLength <=0 || gameLength >=4){
+            System.out.print("Select Game Length:\n1.Short\n2.Medium\n3.Long\nEnter: ");
+            gameLength = in.nextInt();
+        }
 
 
-while (true){
-    blz_intro();
+        while (gameMode){
 
-
-    // exit while loop
-    blz_exit();
-
-   }
-
-}
-
-
-//                                      --------------- functions----------------
-
-    public static void blz_exit(){
-        Scanner in = new Scanner(System.in);
-        String exit;
-        do {
-            System.out.print("Play Again (y/n): ");
-            exit = in.next().toLowerCase();
-        }while (!exit.equals("n") && !exit.equals("y"));
-        if(exit.equals("n")) {
-            System.out.println("Game Over");
-            System.exit(0);
 
 
         }
-    }
-
-    public static void blz_intro(){
-        Scanner in = new Scanner(System.in);
-        int numPlayers;
-        do {
-            System.out.print("\n\nHow many players: ");
-            numPlayers = in.nextInt();
-
-        }while(numPlayers <=1 || numPlayers >= 5);
-        int gameLength;
-
-        int[] blz_PlayerCoins = new int[numPlayers]; // an array that stores the amount of coins users have
-
-        // giving each player 8 coins to start
-        Arrays.fill(blz_PlayerCoins, 8);
-       // System.out.print(Arrays.toString(blz_PlayerCoins));
-
-
-        do {
-            System.out.print("Select Game Length:\n1.Short\n2.Medium\n3.Long\nEnter: ");// come back to this feature
-            gameLength = in.nextInt();
-        }while(gameLength <=0 || gameLength >=4);
-
-
-
-
 
     }
 }
