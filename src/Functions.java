e799e1605bfc2ddbc2a6de760eeff20ee0267cf2
OliverSM95 <olisimm2006@gmail.com>
2023-06-08 10:04:52 -0400

    -Moved all to functions - finsihed start menu

--- src/Functions.java
+++ src/Functions.java
@@ -8,6 +8,16 @@ import java.util.Scanner;
 
 public class Functions {
 
+  // constants
+    private static final int TILE_COUNT = 32;
+    private static final int BOARD_SIZE = 1080; // Board size in pixels
+    private static final int OUTER_MARGIN = BOARD_SIZE / 20; // Outer margin size in pixels
+    private static final int INNER_MARGIN = BOARD_SIZE / 40; // Inner margin size in pixels
+    private static final Color[] TILE_COLORS = {Color.BLUE, Color.YELLOW}; // Tile colors (blue and gold)
+
+    public static int userTurn = 1;
+    public static boolean extraPlayers;
+
     public static void main(String[] args) {
 
 
@@ -32,13 +42,13 @@ public class Functions {
             label.setText("Turn: User 1");
         }else if(turn ==2) {
             label.setText("Turn: User 2");
-        } else if (Main.extraPlayers && turn==3) {
+        } else if (extraPlayers && turn==3) {
             label.setText("Turn: User 3");
-        } else if (Main.extraPlayers && turn ==4) {
+        } else if (extraPlayers && turn ==4) {
             label.setText("Turn: User 4");
         }
     }
-
+/*
     public static void blz_exit(){
         Scanner in = new Scanner(System.in);
         String exit;
@@ -64,7 +74,7 @@ public class Functions {
 
         } while (numPlayers.length ==3 || (numPlayers.length <= 1 || numPlayers.length >= 5)); // only even numbers
 
-        if(numPlayers.length >3) Main.extraPlayers = true;
+        if(numPlayers.length >3) extraPlayers = true;
 
         int[] blz_PlayerCoins = new int[numPlayers.length]; // an array that stores the amount of coins users have
 
@@ -73,8 +83,13 @@ public class Functions {
         // System.out.print(Arrays.toString(blz_PlayerCoins));
         return numPlayers;
     }
+*/
 
-public static void IntroMenu(){// intro menu screen
+
+
+                                                         // intro menu
+
+    public static void IntroMenu(){// intro menu screen
     ////
     //Add Start Menu//
     JFrame startFrame = new JFrame("Welcome To LPCI Monopoly");
@@ -82,80 +97,189 @@ public static void IntroMenu(){// intro menu screen
     startPanel.setBackground(Color.decode("#4bd183"));
 
     startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    startFrame.setSize(400, 300);
-    startFrame.setSize(400,400);
+    startFrame.setSize(400, 400);
+    startPanel.setSize(400,400);
+    startPanel.setLayout(null);
 
-    JButton startButton1 = new JButton("Start Game");
+    JButton startButton = new JButton("Start");
 
-    startButton1.setBounds(100,100,100, 40);
+    startButton.setBounds(20,10,85, 30);
 
     //startButton1.setLocation(0,100);
 
 
-    startButton1.addActionListener(new ActionListener(){// button add action listener
+    JButton exitButton = new JButton("Exit");
+
+    exitButton.setBounds(280,10,85, 30);
+
+    JButton creditsButton = new JButton("Credits");
+
+    creditsButton.setBounds(150,10,85, 30);
+
+
+    startButton.addActionListener(new ActionListener(){// button add action listener
         public void actionPerformed(ActionEvent e){
             //
             startPanel.removeAll();  // Remove all components from the panel
             startPanel.revalidate(); // Revalidate the panel to update the layout
             startPanel.repaint();    // Repaint the panel to reflect the changes
-            Main.mode = true;
+           gameBoard();
+
+
+            startFrame.dispose();
             // System.out.print(mode);
         }
     });// ends here
+
+    creditsButton.addActionListener(new ActionListener(){// credits button
+        public void actionPerformed(ActionEvent e){
+            //
+            startPanel.removeAll();  // Remove all components from the panel
+            startPanel.revalidate(); // Revalidate the panel to update the layout
+            startPanel.repaint();    // Repaint the panel to reflect the changes
+            JTextArea credits = new JTextArea("Credits");
+            credits.setBounds(150,20,50,20);
+            startPanel.add(credits);
+
+
+        }
+    });// ends here
+
+
+    exitButton.addActionListener(new ActionListener(){// exit button action listener
+        public void actionPerformed(ActionEvent e){
+            //
+            System.exit(914);// user pressed exit
+
+
+        }
+    });// ends here
+
     ImageIcon startImageIcon = new ImageIcon("src/Images/PantherLogo.png"); // Logo Picture
     JLabel startImageLabel = new JLabel(startImageIcon);// assign ImageIcon to a JLabel, to be able to paste to screen.
 
-    startImageLabel.setBounds(0,0,200,245);
+    startImageLabel.setBounds(0,30,403,331);
 
 
 
+    startFrame.add(startPanel);// add the panel into the frame
 
-
-
-    startFrame.add(startPanel);// add the pannel into the frame
-
-    startPanel.add(startButton1);
+    startPanel.add(startButton);
+    startPanel.add(creditsButton);
+    startPanel.add(exitButton);
     startPanel.add(startImageLabel);
-
-
-
-
-
-
+    //frame settings
     startFrame.setResizable(false);// Prevent User from changing the window size
     startFrame.setVisible(true);
+
+
 }
 
+                                                // main game
 
 
-    /*
-    public static int blz_getGameLength(){
-        Scanner in = new Scanner(System.in);
-        final int blz_Short = 16;
-        final int blz_Med = 30;
-        final int blz_Long = 40;
-        int gameLength;
-        do {
-            System.out.print("Select Game Length:\n1.Short\n2.Medium\n3.Long\nEnter: ");// come back to this feature
-            gameLength = in.nextInt();
-        }while(gameLength <=0 || gameLength >=4);
+    public static void gameBoard(){
+        // Create the main frame
+        JFrame frame = new JFrame("Monopoly Board");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null); // Use absolute layout
 
-        if(gameLength == 1){
-            gameLength = blz_Short;
+        // Create the tile panel
+        JPanel tilePanel = new JPanel();
+        tilePanel.setLayout(null); // Use absolute layout
+        tilePanel.setBounds(0, 0, BOARD_SIZE, BOARD_SIZE);
+        frame.add(tilePanel);
 
-        } else if (gameLength==2) {
-            gameLength = blz_Med;
+        // Images //
+        // logo in screen
+        // Create a JLabel to display the image
+        ImageIcon imageIcon = new ImageIcon("src/Images/PantherLogo.png"); // Logo Picture
+        JLabel imageLabel = new JLabel(imageIcon);// assign ImageIcon to a JLabel, to be able to paste to screen.
 
-        }else{
-            gameLength = blz_Long;
+        imageLabel.setSize(600, 645);
+        imageLabel.setLocation(BOARD_SIZE / 5, BOARD_SIZE / 5);
+        tilePanel.add(imageLabel);
+
+        ImageIcon chestIcon = new ImageIcon(("src/Images/ChestCard.png"));
+        JLabel chestLabel = new JLabel(chestIcon); // Making image a J label
+        chestLabel.setSize(350, 450);
+        chestLabel.setLocation(350, 57);
+
+        tilePanel.add(chestLabel);
+
+
+        //////////////
+
+        // Create the tiles
+        JButton[] tiles = new JButton[TILE_COUNT];
+        int tileSize = (BOARD_SIZE - 2 * OUTER_MARGIN - 8 * INNER_MARGIN) / 9;
+        int row = 0;
+        int col = 0;
+        int colorIndex = 0; // Index for tile colors
+        int i = 0;
+        while (i < TILE_COUNT - 1) {
+            if (row == 0 || row == 8 || col == 0 || col == 8) {
+                tiles[i] = new JButton(String.valueOf(i + 1));
+                tiles[i].setBounds(OUTER_MARGIN + col * (tileSize + INNER_MARGIN),
+                        OUTER_MARGIN + row * (tileSize + INNER_MARGIN), tileSize, tileSize);
+                tiles[i].setHorizontalAlignment(SwingConstants.RIGHT); // Set number alignment to right
+                tiles[i].setVerticalAlignment(SwingConstants.TOP); // Set number alignment to top
+                tiles[i].setEnabled(false); // Disable the tile button
+                tiles[i].setBackground(TILE_COLORS[colorIndex]); // Set tile color
+                tilePanel.add(tiles[i]);
+                i++;
+            }
+            if (col < 8 && row == 0) {
+                col++;
+            } else if (row < 8 && col == 8) {
+                row++;
+            } else if (col > 0 && row == 8) {
+                col--;
+            } else if (col == 0 && row > 1) {
+                row--;
+            }
+            colorIndex = (colorIndex + 1) % TILE_COLORS.length; // Update tile color index
         }
 
+        // Add the additional tile on the left side
+        tiles[TILE_COUNT - 1] = new JButton(String.valueOf(TILE_COUNT));
+        tiles[TILE_COUNT - 1].setBounds(OUTER_MARGIN, OUTER_MARGIN + tileSize + INNER_MARGIN, tileSize, tileSize);
+        tiles[TILE_COUNT - 1].setHorizontalAlignment(SwingConstants.RIGHT); // Set number alignment to right
+        tiles[TILE_COUNT - 1].setVerticalAlignment(SwingConstants.TOP); // Set number alignment to top
+        tiles[TILE_COUNT - 1].setEnabled(false); // Disable the additional tile button
+        tiles[TILE_COUNT - 1].setBackground(TILE_COLORS[colorIndex]); // Set tile color
+        tilePanel.add(tiles[TILE_COUNT - 1]);
 
-        return gameLength;
+
+        //Game Top Bar Menu//
+        JFrame MenuFrame = new JFrame("Game Menu");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //  frame.setSize(400, 300);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("GameInfo");
+
+        JMenuItem menuItem1 = new JMenuItem("Credits");
+        JMenuItem menuItem2 = new JMenuItem("Don't Click");
+        menu.add(menuItem1);
+        menu.add(menuItem2);
+
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
 
 
+        ///
+
+
+        // Set the frame size and visibility
+        tilePanel.setBackground(Color.decode("#ad66d9"));// set background color (pastel violet)
+        frame.setResizable(false);// Prevent User from changing the window size
+        frame.setSize(BOARD_SIZE, BOARD_SIZE);// set board size
+        frame.setVisible(true);
     }
 
-     */
 
-}
+
+
+
+}// end of class
--- src/Main.java
+++ src/Main.java
@@ -3,122 +3,15 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Main {
-    private static final int TILE_COUNT = 32;
-    private static final int BOARD_SIZE = 1080; // Board size in pixels
-    private static final int OUTER_MARGIN = BOARD_SIZE / 20; // Outer margin size in pixels
-    private static final int INNER_MARGIN = BOARD_SIZE / 40; // Inner margin size in pixels
-    private static final Color[] TILE_COLORS = {Color.BLUE, Color.YELLOW}; // Tile colors (blue and gold)
 
-    public static int userTurn =1;
-    public static boolean extraPlayers;
-
-    public static boolean mode = false;
 
     public static void main(String[] args) {
-        // Create the main frame
-        JFrame frame = new JFrame("Monopoly Board");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null); // Use absolute layout
 
-        // Create the tile panel
-        JPanel tilePanel = new JPanel();
-        tilePanel.setLayout(null); // Use absolute layout
-        tilePanel.setBounds(0, 0, BOARD_SIZE, BOARD_SIZE);
-        frame.add(tilePanel);
-
-                                                        // Images //
-        // logo in screen
-        // Create a JLabel to display the image
-        ImageIcon imageIcon = new ImageIcon("src/Images/PantherLogo.png"); // Logo Picture
-        JLabel imageLabel = new JLabel(imageIcon);// assign ImageIcon to a JLabel, to be able to paste to screen.
-
-        imageLabel.setSize(600,645);
-        imageLabel.setLocation(BOARD_SIZE/5,BOARD_SIZE/5);
-        tilePanel.add(imageLabel);
-
-ImageIcon chestIcon = new ImageIcon(("src/Images/ChestCard.png"));
-JLabel chestLabel = new JLabel(chestIcon); // Making image a J label
-        chestLabel.setSize(350,450);
-        chestLabel.setLocation(350,57);
-
-        tilePanel.add(chestLabel);
-
-
-        //////////////
-
-        // Create the tiles
-        JButton[] tiles = new JButton[TILE_COUNT];
-        int tileSize = (BOARD_SIZE - 2 * OUTER_MARGIN - 8 * INNER_MARGIN) / 9;
-        int row = 0;
-        int col = 0;
-        int colorIndex = 0; // Index for tile colors
-        int i = 0;
-        while (i < TILE_COUNT - 1) {
-            if (row == 0 || row == 8 || col == 0 || col == 8) {
-                tiles[i] = new JButton(String.valueOf(i + 1));
-                tiles[i].setBounds(OUTER_MARGIN + col * (tileSize + INNER_MARGIN),
-                        OUTER_MARGIN + row * (tileSize + INNER_MARGIN), tileSize, tileSize);
-                tiles[i].setHorizontalAlignment(SwingConstants.RIGHT); // Set number alignment to right
-                tiles[i].setVerticalAlignment(SwingConstants.TOP); // Set number alignment to top
-                tiles[i].setEnabled(false); // Disable the tile button
-                tiles[i].setBackground(TILE_COLORS[colorIndex]); // Set tile color
-                tilePanel.add(tiles[i]);
-                i++;
-            }
-            if (col < 8 && row == 0) {
-                col++;
-            } else if (row < 8 && col == 8) {
-                row++;
-            } else if (col > 0 && row == 8) {
-                col--;
-            } else if (col == 0 && row > 1) {
-                row--;
-            }
-            colorIndex = (colorIndex + 1) % TILE_COLORS.length; // Update tile color index
-        }
-
-        // Add the additional tile on the left side
-        tiles[TILE_COUNT - 1] = new JButton(String.valueOf(TILE_COUNT));
-        tiles[TILE_COUNT - 1].setBounds(OUTER_MARGIN, OUTER_MARGIN + tileSize + INNER_MARGIN, tileSize, tileSize);
-        tiles[TILE_COUNT - 1].setHorizontalAlignment(SwingConstants.RIGHT); // Set number alignment to right
-        tiles[TILE_COUNT - 1].setVerticalAlignment(SwingConstants.TOP); // Set number alignment to top
-        tiles[TILE_COUNT - 1].setEnabled(false); // Disable the additional tile button
-        tiles[TILE_COUNT - 1].setBackground(TILE_COLORS[colorIndex]); // Set tile color
-        tilePanel.add(tiles[TILE_COUNT - 1]);
-
-
-                                                //Game Top Bar Menu//
-            JFrame MenuFrame = new JFrame("Game Menu");
-       // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      //  frame.setSize(400, 300);
-
-            JMenuBar menuBar = new JMenuBar();
-            JMenu menu = new JMenu("GameInfo");
-
-            JMenuItem menuItem1 = new JMenuItem("Credits");
-            JMenuItem menuItem2 = new JMenuItem("Don't Click");
-        menu.add(menuItem1);
-        menu.add(menuItem2);
-
-        menuBar.add(menu);
-        frame.setJMenuBar(menuBar);
 
         Functions.IntroMenu();// call menu function
 
-
-
-                                                    ///
-
-
-
-
-        // Set the frame size and visibility
-        tilePanel.setBackground(Color.decode("#ad66d9"));// set background color (pastel violet)
-        frame.setResizable(false);// Prevent User from changing the window size
-        frame.setSize(BOARD_SIZE, BOARD_SIZE);// set board size
-        frame.setVisible(true);
+    }
     }
 
 
-}
 
